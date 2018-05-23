27af34280dc0885b14810a6cb1bbcd20ffe09a0b
xiaoyu311 <616872074@qq.com>
2018-05-23 16:50:03 +0800

    设置session

--- router/index.js
+++ router/index.js
@@ -9,6 +9,7 @@ export default app => {
   app.use('/sign', sign);
   app.use('/user', user);
   app.get('/getcookie', (req, res) => { 
+    req.session.set = req.sessionID;
     console.log(req.sessionID)
     res.send({ status: 1, message: '获取cookie成功' });
   });
